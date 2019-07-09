f1601f71d131bf8fce71603ce20a76ece57ebeef
Petr Komlev <petr.komlev@gmail.com>
2019-07-09 14:24:20 +0300

    add test routing registration, auth, logout functions

--- src/index.js
+++ src/index.js
@@ -8,8 +8,281 @@ import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 import interactionPlugin from '@fullcalendar/interaction';
 import ruLocale from '@fullcalendar/core/locales/ru';
 import moment from 'moment';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './style.scss';
+
+class App extends React.Component {
+  state = {
+    userId: ''
+  }
+
+  componentWillMount = () => {
+    if(!this.state.userId)
+      this.auth();
+  }
+
+  auth = () => {
+    fetch('/fc-test/api/auth')
+      .then(response => response.json())
+      .then(result => {
+        if(result.error) {
+          console.log(result.error);
+          return;
+        }
+
+        this.setState({userId: result.id})
+      })
+      .catch(e => console.log(e));
+  }
+
+  logout = event => {
+    event.preventDefault();
+    
+    fetch('/fc-test/api/logout')
+      .then(response => response.json())
+      .then(result => {
+        if(result.error) {
+          console.log(result.error);
+          return;
+        }
+
+        this.setState({userId: ''})
+      })
+      .catch(e => console.log(e));
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Router>
+          <div className="section section__left">
+            <div>
+              
+              {!this.state.userId ? (
+                <ul>
+                  <li>
+                    <Link to="/fc-test/registration">Registration</Link>
+                  </li>
+                  <li>
+                    <Link to="/fc-test/login">Login</Link>
+                  </li>
+                </ul>
+              ) : (
+                <ul>
+                  <li>
+                    <Link to="/fc-test">Calendar</Link>
+                  </li>
+                  <li>
+                    <a href="#" onClick={this.logout}>Logout</a>
+                  </li>
+                </ul>
+              )}
+                
+            </div>
+          </div>
+          <div className="section section__right">
+            <div className="wrapper">
+              <Route exact path="/fc-test" component={Calendar} />
+              <Route path="/fc-test/registration" component={Registration} />
+              <Route path="/fc-test/login" component={Login} />
+            </div>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+}
+
+class Registration extends React.Component {
+  state = {
+    userEmail: '',
+    userPassword: ''
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const name = target.name;
+    let value = '';
+
+    if(target.type === 'checkbox')
+      value = target.checked;
+    else if(target.type === 'select-multiple') {
+      const options = target.options;
+      value = [];
+      for(let i = 0; i < options.length; i++) {
+        if(options[i].selected) 
+          value.push(options[i].value);
+      }
+    }
+    else
+      value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  addUser = event => {
+    event.preventDefault();
+
+    if(!this.state.userEmail.trim().length || !this.state.userPassword.trim().length)
+      return false;
+
+    fetch('/fc-test/api/users', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.userEmail,
+        password: this.state.userPassword
+      }),
+      headers: {'content-type': 'application/json'}
+    })
+    .then(function(response) {
+      console.log(response.status);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data)
+    })
+    .catch(alert);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="section section__left">
+          <div className="section__form">
+            <h3>Registration</h3>
+            <form onSubmit={this.addUser}>
+              <p>
+                <label>
+                  Email: <br />
+                  <input 
+                    type="email"
+                    name="userEmail" 
+                    value={this.state.userEmail} 
+                    onChange={this.handleInputChange} 
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Password: <br />
+                  <input 
+                    type="password"
+                    name="userPassword" 
+                    value={this.state.userPassword}  
+                    onChange={this.handleInputChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <input type="submit" value="Registration" />
+              </p>
+            </form>
+          </div>
+        </div>
+        <div className="section section__right">
+        </div>
+      </>
+    );
+  }
+}
+
+class Login extends React.Component {
+  state = {
+    userEmail: '',
+    userPassword: ''
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const name = target.name;
+    let value = '';
+
+    if(target.type === 'checkbox')
+      value = target.checked;
+    else if(target.type === 'select-multiple') {
+      const options = target.options;
+      value = [];
+      for(let i = 0; i < options.length; i++) {
+        if(options[i].selected) 
+          value.push(options[i].value);
+      }
+    }
+    else
+      value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  login = event => {
+    event.preventDefault();
+
+    if(!this.state.userEmail.trim().length || !this.state.userPassword.trim().length)
+      return false;
+
+    fetch('/fc-test/api/auth', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.state.userEmail,
+        password: this.state.userPassword
+      }),
+      headers: {'content-type': 'application/json'}
+    })
+    .then(function(response) {
+      console.log(response.status);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data)
+    })
+    .catch(alert);
+  }
+
+  render() {
+    return (
+      <>
+        <div className="section section__left">
+          <div className="section__form">
+            <h3>Login</h3>
+            <form onSubmit={this.login}>
+              <p>
+                <label>
+                  Email: <br />
+                  <input 
+                    type="email"
+                    name="userEmail" 
+                    value={this.state.userEmail} 
+                    onChange={this.handleInputChange} 
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Password: <br />
+                  <input 
+                    type="password"
+                    name="userPassword" 
+                    value={this.state.userPassword}  
+                    onChange={this.handleInputChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <input type="submit" value="Login" />
+              </p>
+            </form>
+          </div>
+        </div>
+        <div className="section section__right">
+        </div>
+      </>
+    );
+  }
+}
 
 class Calendar extends React.Component {
   state = {
@@ -295,9 +568,9 @@ class Calendar extends React.Component {
 
   render() {
     return (
-      <div className="wrapper">
+      <>
         <div className="section section__left">
-          <div className="sectin__form">
+          <div className="section__form">
             <h3>Add Resource</h3>
             <form onSubmit={this.addSource}>
               <p>
@@ -337,7 +610,7 @@ class Calendar extends React.Component {
               </p>
             </form>
           </div>
-          <div className="sectin__form">
+          <div className="section__form">
             <h3>{this.state.eventEditing ? 'Edit' : 'Add'} Event</h3>
             <form onSubmit={this.state.eventEditing ? this.updateEvent : this.addEvent}>
               <p>
@@ -445,12 +718,12 @@ class Calendar extends React.Component {
             events={this.state.events}
           />
         </div>
-      </div>
+      </>
     );
   }
 }
 
 ReactDOM.render(
-  <Calendar />,
+  <App />,
   document.getElementById('root')
 );
