b00959284a9b7910698d7efa49bab5d82a2f7603
Petr Komlev <petr.komlev@gmail.com>
2019-11-26 15:13:24 +0300

    updated package and fix rules for fonts in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         ],
       },
       {
-        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         use: [{
           loader: 'file-loader',
           options: {
